57df0be44cece2a0723da233013355056e9c011b
St1Ff90 <t0503397771@gmail.com>
2022-08-04 14:30:03 +0300

    fixup! Add project files.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DelegateHW;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Sort(User[] users, Func<User, User, int> comparer)
+        {
+            for (int i = 0; i < users.Length; i++)
+            {
+                for (int j = i + 1; j < users.Length; j++)
+                {
+                    if (comparer(users[i], users[j]) > 0)
+                    {
+                        var temp = users[i];
+                        users[i] = users[j];
+                        users[j] = temp;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            User[] users = new User[3]
+            {
+                new User("Vasia","Pupkin", DateTime.Now, "123"),
+                new User("Kolia","Petechkin", DateTime.Now, "321"),
+                new User("Seergay","Andreyev", DateTime.Now, "222"),
+            };
+
+            Sort(users, User.CompareDateOfBirthday);
+        }
+    }
+}
