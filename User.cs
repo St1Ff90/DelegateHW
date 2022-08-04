bc982390a6c92eaf8cf91ec56b1325765f20ded5
St1Ff90 <t0503397771@gmail.com>
2022-08-04 14:31:33 +0300

    fixup! fixup! Add project files.

--- User.cs
+++ User.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace DelegateHW
 {
-    internal class User
+    internal class User : IComparer<User>
     {
+        public string ?FierstName { get; set; }
+        public string ?LastName { get; set; }
+        public DateTime DateOfBirthday { get; set; }
+        public string ?PhoneNumber { get; set; }
+
+        public User(string fierstName, string lastName, DateTime dateOfBierthday, string phoneNumber)
+        {
+            FierstName = fierstName;
+            LastName = lastName;
+            DateOfBirthday = dateOfBierthday;
+            PhoneNumber = phoneNumber;
+        }
+        
+        public int CompareFierstName(User u1, User u2)
+        {
+            return u1.FierstName.CompareTo(u2.FierstName);
+        }
+
+        public int CompareLastName(User u1, User u2)
+        {
+            return u1.LastName.CompareTo(u2.FierstName);
+        }
+
+        public static int CompareDateOfBirthday(User u1, User u2)
+        {
+            return u1.DateOfBirthday.CompareTo(u2.FierstName);
+        }
+
+        public int ComparePhoneNumber(User u1, User u2)
+        {
+            return u1.PhoneNumber.CompareTo(u2.FierstName);
+        }
+
+        public int Compare(User? x, User? y)
+        {
+            if (object.ReferenceEquals(x, y))
+            {
+                return 0;
+            }
+
+            if (x == null)
+            {
+                return -1;
+            }
+
+            if (y == null)
+            {
+                return 1;
+            }
+
+            return x.DateOfBirthday.CompareTo(y.DateOfBirthday);
+        }
     }
 }
